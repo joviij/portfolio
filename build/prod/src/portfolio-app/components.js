d3202711a45b859c5130db9885ac5b2b70b0044e
Jovin James <jamesjovin23@gmail.com>
2019-03-10 23:47:22 -0700

    Create build prod folder again

--- build/prod/src/portfolio-app/components.js
+++
@@ -1,4 +0,0 @@
-import './components/header-section/header-section.js';
-import './components/custom-folder/custom-folder.js';
-import './components/folder-box/folder-box.js';
-import './components/cool-card/cool-card.js';
