95735b2abddd6e8907fd0ae0f8b594f9abf197a4
Jovin James <jovin.james@gm.com>
2019-03-11 09:44:13 -0700

    Fixed the prod build process via polymer.json

---
+++ src/portfolio-app/components.js
@@ -0,0 +1,4 @@
+import './components/header-section/header-section.js';
+import './components/custom-folder/custom-folder.js';
+import './components/folder-box/folder-box.js';
+import './components/cool-card/cool-card.js';
---
+++ src/portfolio-app/components/custom-file/custom-file.js
@@ -0,0 +1,42 @@
+import { html, PolymerElement } from "../../../../node_modules/@polymer/polymer/polymer-element.js";
+
+class CustomFile extends PolymerElement {
+  static get template() {
+    return html`
+        <style>
+            .file-icon {
+                width: 20px;
+                vertical-align: bottom;
+                padding-right: 5px;
+            }
+
+            .file {
+                cursor: default;
+            }
+        </style>
+        <span on-click="handleFileClick" class="file"><img class="file-icon" src="../../../../../assets/file.svg"/>[[name]]</span>
+        `;
+  }
+
+  static get properties() {
+    return {
+      name: {
+        type: String,
+        value: ""
+      }
+    };
+  }
+
+  handleFileClick() {
+    this.dispatchEvent(new CustomEvent('showCard', {
+      bubbles: true,
+      composed: true,
+      detail: {
+        cardName: this.name
+      }
+    }));
+  }
+
+}
+
+customElements.define('custom-file', CustomFile);
